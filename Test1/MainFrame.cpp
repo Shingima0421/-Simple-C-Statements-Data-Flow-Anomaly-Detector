5baaea6516852d9fcb4120c04076b8a76da0af74
eddie naranbaatar <Naranbaatareddie@gmail.com>
2024-05-14 01:17:25 +0800

    Removed INSERT and DELETE buttons as they will be interpreted as key events instead. Also added seperate events for buttons input parameters.

--- Test1/MainFrame.cpp
+++ Test1/MainFrame.cpp
@@ -3,15 +3,47 @@
 
 MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
 	wxPanel* panel = new wxPanel(this); // Make sure to update the IDs to something recognizable for ease of use later
-	wxButton* insert_button = new wxButton(panel, wxID_ANY, "Insert", wxPoint(306, 30), wxSize(40, 20));
-	wxButton* delete_button = new wxButton(panel, wxID_ANY, "Delete", wxPoint(352, 30), wxSize(40, 20));
+
+	//wxButton* insert_button = new wxButton(panel, wxID_ANY, "Insert", wxPoint(306, 30), wxSize(40, 20));
+	//wxButton* delete_button = new wxButton(panel, wxID_ANY, "Delete", wxPoint(352, 30), wxSize(40, 20));
+	wxButton* modify_button = new wxButton(panel, wxID_ANY, "Modify", wxPoint(306, 29), wxSize(86, 22));
+
+	//insert_button->Bind(wxEVT_BUTTON, &MainFrame::OnInsertButtonClicked, this);
+	//delete_button->Bind(wxEVT_BUTTON, &MainFrame::OnDeleteButtonClicked, this);
+	modify_button->Bind(wxEVT_BUTTON, &MainFrame::OnModifyButtonClicked, this);
 
 	wxStaticText* SimpleCstatement = new wxStaticText(panel, wxID_ANY, "Simple C Statement", wxPoint(17, 10));
 	wxTextCtrl* inputSimpleCstatement = new wxTextCtrl(panel, wxID_ANY, "string", wxPoint(17, 30), wxSize(200, 20));
 
+	inputSimpleCstatement->Bind(wxEVT_TEXT, &MainFrame::OnInsertTextChanged, this);
+
 	wxStaticText* LineNumber = new wxStaticText(panel, wxID_ANY, "Line Number", wxPoint(227, 10));
 	wxTextCtrl* inputLineNumber = new wxTextCtrl(panel, wxID_ANY, "int", wxPoint(227, 30), wxSize(70, 20));
 
+	inputLineNumber->Bind(wxEVT_TEXT, &MainFrame::OnLineNumberChanged, this);
+
 	wxTextCtrl* outputCode = new wxTextCtrl(panel, wxID_ANY, "int main{\n\n\n\n}", wxPoint(17, 60), wxSize(375, 500));
 	wxTextCtrl* outputAnomaly = new wxTextCtrl(panel, wxID_ANY, "No Anomalies detected.", wxPoint(17, 570), wxSize(375,100));
+
+	CreateStatusBar();
+}
+
+/*void MainFrame::OnInsertButtonClicked(wxCommandEvent& evt) {
+	wxLogStatus("Insert Button Clicked!");
+}*/
+
+/*void MainFrame::OnDeleteButtonClicked(wxCommandEvent& evt) {
+	wxLogStatus("Delete Button Clicked!");
+}*/
+
+void MainFrame::OnModifyButtonClicked(wxCommandEvent& evt) {
+	wxLogStatus("Modify Button Clicked!");
+}
+
+void MainFrame::OnInsertTextChanged(wxCommandEvent& evt) {
+	wxLogStatus("Simple C Statement Inserted!");
+}
+
+void MainFrame::OnLineNumberChanged(wxCommandEvent& evt) {
+	wxLogStatus("Line Number Inserted!");
 }
--- Test1/MainFrame.h
+++ Test1/MainFrame.h
@@ -5,4 +5,10 @@ class MainFrame : public wxFrame
 {
 public:
 	MainFrame(const wxString& title);
+
+	void OnInsertButtonClicked(wxCommandEvent& evt);
+	void OnDeleteButtonClicked(wxCommandEvent& evt);
+	void OnModifyButtonClicked(wxCommandEvent& evt);
+	void OnInsertTextChanged(wxCommandEvent& evt);
+	void OnLineNumberChanged(wxCommandEvent& evt);
 };
