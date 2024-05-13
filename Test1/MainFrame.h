3ad2b9916900b9a8493a4302677ac1f149b5fc68
eddie naranbaatar <Naranbaatareddie@gmail.com>
2024-05-14 01:25:32 +0800

    Small changes to MainFrame header file

--- Test1/MainFrame.h
+++ Test1/MainFrame.h
@@ -6,8 +6,8 @@ class MainFrame : public wxFrame
 public:
 	MainFrame(const wxString& title);
 
-	void OnInsertButtonClicked(wxCommandEvent& evt);
-	void OnDeleteButtonClicked(wxCommandEvent& evt);
+	//void OnInsertButtonClicked(wxCommandEvent& evt);
+	//void OnDeleteButtonClicked(wxCommandEvent& evt);
 	void OnModifyButtonClicked(wxCommandEvent& evt);
 	void OnInsertTextChanged(wxCommandEvent& evt);
 	void OnLineNumberChanged(wxCommandEvent& evt);
